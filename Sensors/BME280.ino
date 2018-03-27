133e8acc8ff143b71dfc8769da92973cf53d7b1d
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2018-03-27 18:46:54 -0500

    Add files via upload

---
+++ Sensors/BME280.ino
@@ -0,0 +1,49 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+Adafruit_BME280 bme; // I2C
+int delayTime = 1000;
+
+void setup() {
+    Serial.begin(9600);
+    bool status = bme.begin();  
+    if (!status) {
+        Serial.println("Could not find a valid BME280 sensor, check wiring!");
+        while (1);
+    }
+}
+
+void loop() { 
+    float temp = bme.readTemperature();
+    float pressure = bme.readPressure() / 100.0F;
+    float humidity = bme.readHumidity();
+    if (isnan(temp)) {
+      temp = 0;
+    }
+    if (isnan(pressure)) {
+      pressure = 0;
+    }
+    if (isnan(humidity)) {
+      humidity = 0;
+    }
+    String message = "nuc:temp-" + String(temp) + ":pressure-" + String(pressure) + ":humidity-" + String(humidity) + ";";
+    Serial.println(message);
+    delay(delayTime);
+}
+
+void printValues() {
+    Serial.print("Temperature = ");
+    Serial.print(bme.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print("Pressure = ");
+
+    Serial.print(bme.readPressure() / 100.0F);
+    Serial.println(" hPa");
+
+    Serial.print("Humidity = ");
+    Serial.print(bme.readHumidity());
+    Serial.println(" %");
+}
