1e5c8b621da00df0f4046acfadfb0121e235068c
UiRobotics <31750785+UiRobotics@users.noreply.github.com>
2018-03-27 21:11:12 -0500

    Add files via upload

--- Sensors/BME280.ino
+++ Sensors/BME280.ino
@@ -5,17 +5,27 @@
 
 Adafruit_BME280 bme; // I2C
 int delayTime = 1000;
+bool recieving = false;
 
 void setup() {
     Serial.begin(9600);
-    bool status = bme.begin();  
+    bool status = bme.begin();
     if (!status) {
         Serial.println("Could not find a valid BME280 sensor, check wiring!");
         while (1);
     }
 }
 
+String rec = "";
 void loop() { 
+    while (!recieving){
+      rec = Serial.readString();
+      Serial.print(rec);
+      if (rec == "ready") {
+        recieving = true;
+      }
+      delay(delayTime);
+    }
     float temp = bme.readTemperature();
     float pressure = bme.readPressure() / 100.0F;
     float humidity = bme.readHumidity();
@@ -28,7 +38,7 @@ void loop() {
     if (isnan(humidity)) {
       humidity = 0;
     }
-    String message = "nuc:temp-" + String(temp) + ":pressure-" + String(pressure) + ":humidity-" + String(humidity) + ";";
+    String message = "Nuc:temp-" + String(temp) + ":pressure-" + String(pressure) + ":humidity-" + String(humidity) + ";";
     Serial.println(message);
     delay(delayTime);
 }
